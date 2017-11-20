9c0ad2d4ef55eb25395b72afdbcc4ab44bfa639b
gsrnandan <raghunandang.89@gmail.com>
2017-11-20 19:17:07 +0530

    Update the tempTracker Class

--- tempTrackerClass.py
+++ tempTrackerClass.py
@@ -1,18 +1,29 @@
+""" Defines a tracker class for tracking the input temperatures
+"""
+
 class TempTracker():
-	"""This is a tracker class for temperature. """
+	"""Temperature Tracker class 
+	"""
 
 	def __init__(self):
 		
+		#A list to store the input temperature values
 		self._tempTrackerList = []
 
 
 	def isValid(self,temperature):
-
-			"""This function validates the input type and range
-
-				This takes the temperature value and checks if the temperature is within the 
-				range of (0-110) and if its a valid int type
-			"""
+	     """Method to validate the input temperature type and range
+		 
+		 This method reads the input value and checks if the temperature is within the
+		 range of (0-110) and if its a valid int type
+		 
+		 Args:
+		     temperature (int): The temperature value
+		     
+		 Return:
+		     (bool): Returns True if its a valid input, if not returns False
+		 
+	      """
 			if isinstance(temperature, (int,float,long)):
 				if temperature>=0 and temperature <= 110:
 					return True
@@ -20,10 +31,13 @@ class TempTracker():
 
 
 	def insert(self,*listOfTemps):
-			"""This function inserts a new temperature into the existing list of temperatures
+			"""Method to insert a new temperature into the existing list of temperatures
 
-				This function takes an integer argument and adds it to the tempTrackerList
-			"""
+			    This function takes an integer argument and adds it to the tempTrackerList
+			    
+			    Args:
+			    	listOfTemps (list): List of temperatures 
+			 """
 			for temperature in listOfTemps:
 				if self.isValid(temperature) :
 					self._tempTrackerList.append(int(temperature))
@@ -32,29 +46,37 @@ class TempTracker():
 					print "skipped adding '{0}' as it is either an Invalid temperature type or Range".format(temperature)
 
 	def get_max(self):
-			"""This will return the maximum of all the temperatures in the Tracker
+			"""Method to return the maximum of all the temperatures in the Tracker
 				
 			   This function will return the max of tempTrackerList
-			"""
+			   
+			   Return:
+			   	(int): The maximum value in the list of temperatures
+			 """
 
 			if self._tempTrackerList:
 				return max(self._tempTrackerList)	
 
 
 	def get_min(self):
-			"""This will return the minimum of all the temperatures in the Tracker
+			"""Method to return the minimum of all the temperatures in the Tracker
 				
 			   This function will return the min of tempTrackerList
+			   
+			   Return:
+			   	(int): The minimum value in the list of temperatures
 			"""
 			if self._tempTrackerList:
 				return min(self._tempTrackerList)	
 
 		
 	def get_mean(self):
-			"""" This functions returns the mean of all the temperatures tracked
+			"""" Method to return the mean of all the temperatures tracked
 				
-
-				This function will calculate the mean of all integer values inside the tempTrackerList. The return type is float
+			      This function will calculate the mean of all integer values inside the tempTrackerList. The return type is float
+			      
+			      Return:
+			      	    (float): Returns the average of all the temperatures in the list of temperatures.
 			"""
 
 			if self._tempTrackerList:
@@ -62,3 +84,14 @@ class TempTracker():
 				sumOfElements = float(sum(self._tempTrackerList))
 				mean = (sumOfElements/noOfElements)
 				return mean	
+
+
+if __name__ == '__main__':
+    unittest.main()
+    tt = TempTracker()
+    tt.insert(33,34,108)
+    print tt.get_mean()
+    print tt.get_max()
+    print tt.get_min()
+
+
